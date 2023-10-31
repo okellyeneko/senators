b50df94bac75db7c5406c17e4de71d81e9788338
Eneko O'Kelly <okelly.eneko@gmail.com>
2023-10-31 16:22:33 +0000

    Big update with new functions
    clickSenator function that show more details for each senator when a row is clicked.
    
    Sort of the data before the senators are added to the table. It groups all senators by party by default
    
    Specific message displayed if a party has no leaders.
    
    Code that integrates all the corresponding options for each filter. (e.g: all states for State filter ) in the dropdowns
    
    Other small updates and bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 getSenators() //Function call of getSenators
 
+let senatorsData = [];
+
 async function getSenators() { //defining a async function to fetch data from the senators.json file
   const url = "senators.json"; 
   const response = await fetch(url); 
@@ -7,25 +9,27 @@ async function getSenators() { //defining a async function to fetch data from th
   const senators = alldata.objects //Collecting all the data in variable senators
   displayParty(senators) //Calling displayParty and passing the data in params
   allSenators(senators) //Calling allSenators and passing the data in params
+  filter(senators)
+  clickSenator(senators)
 }
   
 function displayParty (data) { //creating function to display parties information (step 1)
 
   partycounter = {} //Empty object that will store number of members for all parties
   const partiesObj = { //Object that regroups different parties HTML elements
-  Republican: {
-    partyDiv: document.getElementById('reps'),
-    leadersList: document.getElementById('repLeaders'),
-  },
-  Democrat: {
-    partyDiv: document.getElementById('dems'),
-    leadersList: document.getElementById('demLeaders'),
-  },
-  Independent: {
-    partyDiv: document.getElementById('indep'),
-    leadersList: document.getElementById('indLeaders'),
-  },
-};
+    Republican: {
+      partyDiv: document.getElementById('reps'),
+      leadersList: document.getElementById('repLeaders'),
+    },
+    Democrat: {
+      partyDiv: document.getElementById('dems'),
+      leadersList: document.getElementById('demLeaders'),
+    },
+    Independent: {
+      partyDiv: document.getElementById('indep'),
+      leadersList: document.getElementById('indLeaders'),
+    },
+  };
 
   for (let i = 0; i < data.length; i++) { //For loop that iterates through all the passed data (all senators)
     const senator = data[i];  //Delcaring some constants to simplify 
@@ -41,7 +45,8 @@ function displayParty (data) { //creating function to display parties informatio
       var li = document.createElement("li")
       li.textContent = `${senator.leadership_title}: ${senator.person.firstname} ${senator.person.lastname} (${party})` //Passing the senator's info  inside new li
       leadersList.appendChild(li) //appending the new li and its content to the ul
-    }
+    } 
+    
   }
 
   for (party in partycounter) { //for each party in partycounter, insert the name of the party and its counter in corresponding html elements
@@ -49,15 +54,31 @@ function displayParty (data) { //creating function to display parties informatio
     const count = partycounter[party]
     container.querySelector("h1").innerHTML = party;
     container.querySelector("p").innerHTML = `Number of members: ${count}`;
+
+    // condition that checks if a party is empty. If so, it creates a new li that displays a message saying so
+    if (partiesObj[party].leadersList.children.length === 0) {
+      const leadersList = partiesObj[party].leadersList //if true find the current party's leaders html list
+      var li = document.createElement("li")
+      li.textContent = "No leaders for this party"
+      leadersList.appendChild(li) //appending the new li and its content to the ul
+    }
+
   }
 }
 
 function allSenators(data) {
   tablebody = document.getElementById('allsenators')
   attributes = document.getElementById('attributes')
-  
+
+  // Sort data by party
+  data.sort((a, b) => {
+    const partyOrder = { 'Republican': 1, 'Democrat': 2, 'Independent': 3 };
+    return partyOrder[a.party] - partyOrder[b.party];
+  });
+
   for (let i = 0; i < data.length; i++) {
     const senator = data[i]; 
+
     //Delcaring some constants to simplify and storing in an object:
     allAttributes = {
       firstname: senator.person.firstname,
@@ -69,28 +90,31 @@ function allSenators(data) {
     }
     
     var row = document.createElement('tr') //New row
-
-    for (x in allAttributes){ //loop that iterates through all the attributes (x)
-       //for all attributes, create a td and textnode (which stores the attribute value)
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode(allAttributes[x]);
-
-
-      //Append text to cell, cell to row, and row to tablebody
-      cell.appendChild(cellText)
-      row.appendChild(cell)
-      tablebody.appendChild(row)
-    }
-    tablebody.appendChild(row); // Appends the row to the table
+        // Add a click event handler to the row
+    row.addEventListener("click", function () {
+      displaySenatorDetails(senator);
+    });
+      for (x in allAttributes){ //loop that iterates through all the attributes (x)
+        //for all attributes, create a td and textnode (which stores the attribute value)
+       var cell = document.createElement("td");
+       var cellText = document.createTextNode(allAttributes[x]);
+ 
+       //Append text to cell, cell to row, and row to tablebody
+       cell.appendChild(cellText)
+       row.appendChild(cell)
+     }
+     tablebody.appendChild(row); // Appends the row to the table
   }
 }
 
-function filter() {
+function filter(data) {
+  //Function that iterats through the table rows and displays rows according to the selected filters
+  //Declare all variables: 
 
-  var selectParty = document.getElementById("selectParty");
-  var selectState = document.getElementById("selectState");
-  var selectGender = document.getElementById("selectGender");
-  var selectRank = document.getElementById("selectRank");
+  const selectParty = document.getElementById("selectParty");
+  const selectState = document.getElementById("selectState");
+  const selectGender = document.getElementById("selectGender");
+  const selectRank = document.getElementById("selectRank");
 
   var partyFilter = selectParty.value.toUpperCase();
   var stateFilter = selectState.value.toUpperCase();
@@ -100,7 +124,10 @@ function filter() {
   var table = document.getElementById("allsenatorsTable");
   var tr = table.getElementsByTagName("tr");
 
-  for (var i =1; i < tr.length; i++) {
+  //Create a loop that iterates through all the table rows necessary (all except other headers)
+
+  for (var i = 2; i < tr.length; i++) {
+    //Inside the for loop, declare variables for all the different cells corrsponding to the attributes
     td = tr[i].cells;
 
     var tdinParty = td[2].innerText.toUpperCase();
@@ -108,10 +135,112 @@ function filter() {
     var tdinRank = td[4].innerText.toUpperCase();
     var tdinState = td[5].innerText.toUpperCase();
 
-    if (tdinParty.indexOf(partyFilter) > -1 && tdinState.indexOf(stateFilter) > -1 && tdinGender.indexOf(genderFilter) > -1 && tdinRank.indexOf(rankFilter) > -1) {
+    //Create conditions that check if the TD contains a substring or not according to each filter.
+    //If the row doesn't meet all conditions, it changes the CSS to display none for that row
+
+    if (
+    tdinParty.indexOf(partyFilter) > -1 && 
+    tdinState.indexOf(stateFilter) > -1 && 
+    (!genderFilter || tdinGender.indexOf(genderFilter) === 0) && 
+    tdinRank.indexOf(rankFilter) > -1) {
       tr[i].style.display = ""
     } else {
       tr[i].style.display = "none"
     }
   }
+
+  //
+  parties = []
+  genders = []
+  ranks = []
+  states = []
+  for (let i = 0; i < data.length; i++) {
+    const senator = data[i];
+    const party = senator.party;
+    const gender = senator.person.gender;
+    const rank = senator.senator_rank;
+    const state = senator.state;
+    
+    if (!parties.includes(party)) {
+      parties.push(party);
+    }
+
+    if (!genders.includes(gender)) {
+        genders.push(gender);
+    }
+
+    if (!ranks.includes(rank)) {
+        ranks.push(rank);
+    }
+
+    if (!states.includes(state)) {
+        states.push(state);
+    }
+  }
+  for (i in parties){
+    var option = document.createElement("option");
+    var optionText = document.createTextNode(parties[i]);
+    option.appendChild(optionText)
+    selectParty.appendChild(option)
+  }
+  for (i in genders){
+    var option = document.createElement("option");
+    var optionText = document.createTextNode(genders[i]);
+    option.appendChild(optionText)
+    selectGender.appendChild(option)
+  }
+  for (i in ranks){
+    var option = document.createElement("option");
+    var optionText = document.createTextNode(ranks[i]);
+    option.appendChild(optionText)
+    selectRank.appendChild(option)
+  }
+  for (i in states){
+    var option = document.createElement("option");
+    var optionText = document.createTextNode(states[i]);
+    option.appendChild(optionText)
+    selectState.appendChild(option)
+  }
+}
+
+function displaySenatorDetails(senator) {
+  const detailsDiv = document.getElementById("detailsDiv");
+  const detailsList = document.getElementById('listofDetails')
+  detailsList.innerHTML = "";
+  
+  const office = document.createElement("li");
+  office.textContent = `Office: ${senator.extra.office}`;
+
+  const birthday = document.createElement("li");
+  birthday.textContent = `Date of birth: ${senator.person.birthday}`;
+
+  const startdate = document.createElement("li");
+  startdate.textContent = `Start date: ${senator.startdate}`;
+
+  const twitter = document.createElement("li");
+  twitter.textContent = `Twitter: ${senator.person.twitterid}`;
+  
+  const youtube = document.createElement("li");
+  youtube.textContent = `Youtube: ${senator.person.youtubeid}`;
+
+  const website = document.createElement("li");
+  website.textContent = "Website: "
+  const websiteUrl = document.createElement("a");
+  websiteUrl.href = senator.website;
+  websiteUrl.textContent = senator.website;
+  websiteUrl.setAttribute('target', "_blank")
+  website.appendChild(websiteUrl);
+
+
+  // Add other details as needed...
+
+  detailsList.appendChild(office);
+  detailsList.appendChild(birthday);
+  detailsList.appendChild(startdate);
+  detailsList.appendChild(twitter);
+  detailsList.appendChild(youtube);
+  detailsList.appendChild(website);
+
+  // Show the details section
+  detailsDiv.style.display = "block";
 }
